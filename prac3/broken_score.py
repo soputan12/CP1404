8107046066be0dfd384738bb344129b92df0240a
Bryant <soputantwelve@gmail.com>
2021-11-24 15:05:14 +0800

    Added functions

--- prac3/broken_score.py
+++ prac3/broken_score.py
@@ -2,16 +2,22 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+def main():
+    score = float(input("Enter score: "))
+    sorted_score = sort_score(score)
+    print(sorted_score)
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    elif score > 90:
-        print("Excellent")
-    elif score > 50:
-        print("Passable")
-    elif score < 50:
-        print("Bad")
+def sort_score(score):
+    if score < 0:
+        return "Invalid score"
+    else:
+        if score > 100:
+            return "Invalid score"
+        elif score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        elif score < 50:
+            return "Bad"
+
+main()
