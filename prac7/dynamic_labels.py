d5fbcdac659b826051044632d7907d7db0e4cda9
Bryant <soputantwelve@gmail.com>
2022-01-04 20:19:03 +0800

    Added Files

---
+++ prac7/dynamic_labels.py
@@ -0,0 +1,35 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
+
+
+class DynamicLabelsApp(App):
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.label_names = {"Mr. Bean", "Johnny English", "Rowan Atkinson"}
+
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        for name in self.label_names:
+            temp_label = Label(text=name)
+            temp_label.bind(on_release=self.press_entry)
+            self.root.ids.entries_box.add_widget(temp_label)
+
+    def press_entry(self, instance):
+        name = instance.text
+        self.status_text = "{}'s number is {}".format(name, self.name[name])
+
+    def clear_all(self):
+        self.root.ids.entries_box.clear_widgets()
+
+
+DynamicLabelsApp().run()
