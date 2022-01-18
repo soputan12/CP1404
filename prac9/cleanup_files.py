1b75452f84fb7a9d36c7ffdca8fee7b169c9b1ae
Bryant <soputantwelve@gmail.com>
2022-01-18 21:08:32 +0800

    Could not add more separators just send

--- prac9/cleanup_files.py
+++ prac9/cleanup_files.py
@@ -3,19 +3,19 @@ import os
 
 
 def main():
-    print("Starting directory is: {}".format(os.getcwd()))
+    print(f"Starting directory is: {os.getcwd()}")
     os.chdir('Lyrics')
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+    print(f"Files in {os.getcwd()}:\n{os.listdir('.')}\n")
     for directory, subdirectory, files in os.walk('.'):
         for filename in files:
             new_name = get_fixed_filename(filename)
-            print("Renaming {} to {}".format(filename, new_name))
+            print(f"Renaming {filename} to {new_name}")
             full_name = os.path.join(directory, filename)
             new_name = os.path.join(directory, new_name)
             os.rename(full_name, new_name)
 
 def get_fixed_filename(filename):
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt") #INCOMPLETE REQUIRES LOWER TO UPPER, SEPARATE IF UPPER
     return new_name
 
 
